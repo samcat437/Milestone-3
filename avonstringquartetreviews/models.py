25abe93f92342d908630863e393ffb04bfd28210
samcat437 <sdrees12@gmail.com>
2022-07-07 13:03:21 +0000

    working on solving redirect issue when submitting review, most likely due to the db not being updated when I changed the columns

--- avonstringquartetreviews/models.py
+++ avonstringquartetreviews/models.py
@@ -2,12 +2,16 @@ from avonstringquartetreviews import db
 
 
 class Review(db.Model):
-    #schema for the Review model
+    # schema for the Review model
     id = db.Column(db.Integer, primary_key=True)
-    review_name = db.Column(db.String(50), unique=True, nullable=False)
+    review_name = db.Column(db.String(25), unique=True, nullable=False)
+    review_first_name = db.Column(db.String(25), unique=True, nullable=False)
+    review_last_name = db.Column(db.String(25), unique=True, nullable=False)
+    review_email = db.Column(db.String(25), unique=True, nullable=False)
     review_date = db.Column(db.Date, nullable=False)
-    review_venue = db.Column(db.String(50), unique=False)
+    review_venue = db.Column(db.String(25), unique=False)
     review_content = db.Column(db.Text, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(), nullable=False)
 
     def __repr__(self): 
         # __repr__ to represent itself in the form of a string
--- avonstringquartetreviews/routes.py
+++ avonstringquartetreviews/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from avonstringquartetreviews import app, db
 from avonstringquartetreviews.models import Review
 
@@ -6,3 +6,28 @@ from avonstringquartetreviews.models import Review
 @app.route("/")
 def home():
     return render_template("reviews.html")
+
+
+@app.route("/reviews")
+def reviews():
+    reviews = list(Review.query.order_by(Review.review_venue).all())
+    return render_template("reviews.html", reviews=reviews)
+
+
+@app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    if request.method == "POST":
+        review = Review(
+            review_name=request.form.get("review_name"),
+            review_first_name=request.form.get("review_first_name"),
+            review_last_name=request.form.get("review_last_name"),
+            review_date=request.form.get("review_date"),
+            review_email=request.form.get("review_email"),
+            review_venue=request.form.get("review_venue"),
+            review_content=request.form.get("review_venue")
+        )
+        db.session.add(review)
+        db.session.commit()
+        # not redirecting 
+        return redirect(url_for("reviews")) 
+    return render_template("add_review.html")
