f2760efe0170212d93c54fdadcf77b279cfbe12e
samcat437 <sdrees12@gmail.com>
2022-07-11 12:55:14 +0000

    added requirements.txt, Procfile, and mongo.py files in order to connect to Mongodb and connect to Heroku

---
+++ mongo.py
@@ -0,0 +1,47 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "avonSQReviews"
+COLLECTION = "review"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+documents = coll.find()
+
+new_docs = [{
+    "first": "terry",
+    "last": "pratchett",
+    "dob": "28/04/1948",
+    "gender": "m",
+    "hair_color": "not much",
+    "occupation": "writer",
+    "nationality": "british"
+}, {
+    "first": "george",
+    "last": "rr martin",
+    "dob": "20/09/1948",
+    "gender": "m",
+    "hair_color": "white",
+    "occupation": "writer",
+    "nationality": "american"
+}]
+coll.insert_many(new_docs)
+
+for doc in documents:
+    print(doc)
--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
+
 from avonstringquartetreviews import app
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
-    )
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
