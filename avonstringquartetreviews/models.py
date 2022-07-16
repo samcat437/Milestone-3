77bd24ddcde9e52806f6bdef1815686a7c1f3eb5
samcat437 <sdrees12@gmail.com>
2022-07-16 09:40:34 +0000

    form posting, however, need to mix functions to populate my_wedding page with form data

--- avonstringquartetreviews/models.py
+++ avonstringquartetreviews/models.py
@@ -23,15 +23,15 @@ class Review(db.Model):
 class Details(db.Model):
     # schema for the Details model
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
+    username = db.Column(db.String(25), unique=False, nullable=False)
     event_name = db.Column(db.String(25), unique=True, nullable=False)
     event_first_name = db.Column(db.String(25), unique=True, nullable=False)
     event_last_name = db.Column(db.String(25), unique=True, nullable=False)
     event_email = db.Column(db.String(25), unique=True, nullable=False)
     event_date = db.Column(db.Date, nullable=False)
     event_venue = db.Column(db.String(25), unique=False)
-    event_start = db.Column(db.Time, unique=False)
-    event_end = db.Column(db.Time, unique=False)
+    event_start = db.Column(db.String(25), unique=False)
+    event_end = db.Column(db.String(25), unique=False)
     event_content = db.Column(db.Text, nullable=False)
 
 
