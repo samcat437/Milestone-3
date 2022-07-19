55816544ce59e9aae0317fcc5c12e9a83c0ebabe
samcat437 <sdrees12@gmail.com>
2022-07-19 23:50:18 +0000

    changing models.py to line too long as heroku crashed

--- avonstringquartetreviews/models.py
+++ avonstringquartetreviews/models.py
@@ -37,8 +37,7 @@ class Details(db.Model):
 
     def __repr__(self): 
         # __repr__ to represent itself in the form of a string
-        return f"#{0} - Event Name: {1} | Date: {2} | Start: {3} | End: {4}"
-        .format(
+        return f"#{0} - Event Name: {1} | Date: {2} | Start: {3} | End: {4}".format(
             self.id, self.event_name, self.event_date,
             self.event_start, self.event_end
         )
